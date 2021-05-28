ef37d7f44f71109e123430863f64e495ddb0495f
cdimitri <cdimitri@wn002.baf.physik.uni-bonn.de>
2021-05-28 18:54:20 +0200

    adding some fixes so that TopoClusterClassifierLargerCNN works

--- util/ml_util.py
+++ util/ml_util.py
@@ -124,7 +124,8 @@ def setupPionData(inputpath, rootfiles, branches = []):
         for rfile in rootfiles
     }
     pdata = {
-        ifile: itree.pandas.df(branches, flatten=False)
+        ifile: pd.DataFrame(itree.arrays(expressions=branches, library='np'))
+        #ifile: itree.pandas.df(branches, flatten=False)
         for ifile, itree in trees.items()
     }
 
@@ -151,18 +152,20 @@ def splitFrameTVT(frame, trainlabel='train', trainfrac = 0.8, testlabel='test', 
     frame[vallabel]   = frame.index.isin(val_index)
 
 
-def setupCells(tree, layer, nrows = -1, flatten=True, energyScale = '', energyFrame = 0):
-    array = tree.array(layer)
-
+def setupCells(tree, layer, nrows = -1, indices = [], flatten=True, energyScale = '', energyFrame = 0):
+    #array = tree.array(layer)
+    array = tree.arrays([layer], library='np')[layer]
     # rescale images to the specified energy variable from the specified frame
     # needs to reshape to n,1,1 to get the clusters, x, y shape of the array
     # numpy handles the multiplication properly if given this shape
     if energyScale != '':
         energy = energyFrame[energyScale].to_numpy().reshape(len(energyFrame),1,1)
-        array = np.multiply(array, energy) # this didn't work, have to do it element by element
-
+        #array = np.multiply(array, energy) # this didn't work, have to do it element by element
+        
     if nrows > 0:
         array = array[:nrows]
+    elif len(indices) > 0:
+        array = array[indices]
     num_pixels = cell_meta[layer]['len_phi'] * cell_meta[layer]['len_eta']
     if flatten:
         array = array.reshape(len(array), num_pixels)
